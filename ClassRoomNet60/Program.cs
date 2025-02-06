2a4949b82040bd8f72964607799445baa460ae47
Samet Secilmis <sas007@edu.zealand.dk>
2025-02-06 13:13:13 +0100

    Student klasse og Properties

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+public class student
+{
+   public  string Name { get; set; }
+        
+    public int BirthMonth;
+
+    public int BirthDay;
+
+}
